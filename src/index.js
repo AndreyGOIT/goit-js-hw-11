e135a371fd7f9c5a93550d4fe7d5f72579d007de
AndreyGOIT <ae.borga@gmail.com>
2022-10-25 15:15:24 +0300

    new try

--- src/index.js
+++ src/index.js
@@ -7,14 +7,105 @@ import SimpleLightbox from 'simplelightbox';
 // Дополнительный импорт стилей
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-// В качестве бэкенда используй публичный API сервиса Pixabay.
-// Зарегистрируйся, получи свой уникальный ключ доступа и ознакомься с документацией.
+const inputField = document.querySelector('input');
+const fetchBtn = document.querySelector('button');
+const listOfGallery = document.querySelector('.list');
+const loadMoreBtn = document.querySelector('.load-more');
 
-//Список параметров строки запроса которые тебе обязательно необходимо указать:
+loadMoreBtn.style.display = 'none';
+// // В качестве бэкенда используй публичный API сервиса Pixabay.
+// // Зарегистрируйся, получи свой уникальный ключ доступа и ознакомься с документацией.
+const BASE_URL = 'https://pixabay.com/api/';
+const API = '30800169-3713389dad872250f057e0e33';
 
+// let pageToFetch = 0;
+let keyword = '';
+
+fetchBtn.addEventListener('click', clickHandler);
+// //Список параметров строки запроса которые тебе обязательно необходимо указать:
+async function clickHandler() {
+  keyword = inputField.value;
+  console.log(keyword);
+  try {
+    const response = await axios.get(`${BASE_URL}`, {
+      params: {
+        apikey: API,
+        q: keyword,
+        image_type: photo,
+        orientation: horizontal,
+        safesearch: true,
+      },
+    });
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// async function fetchEvent() {
+//   keyword = inputField.value;
+//   console.log(keyword);
+
+//   const params = new URLSearchParams({
+//     apikey: API,
+//     q: keyword,
+//     image_type: photo,
+//     orientation: horizontal,
+//     safesearch: true,
+//   });
+
+//   const response = await fetch(`${BASE_URL}?${params}`);
+//   const users = await response.json();
+//   console.log(users);
+//   return users;
+// }
+
+// async function clickHandler() {
+//   try {
+//     const users = await fetchEvent();
+//     console.log(users);
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// }
+
+// function fetchEvent() {
+//   keyword = inputField.value;
+//   console.log(keyword);
+//   const params = new URLSearchParams({
+//     apikey: API,
+//     q: keyword,
+//     image_type: photo,
+//     orientation: horizontal,
+//     safesearch: true,
+//   });
+
+//   return fetch(`${BASE_URL}?${params}`)
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error(response.status);
+//       }
+//       return response.json();
+//     })
+//     .then(elements => {
+//       const markup = elements.map(
+//         ({ likes, views, comments, downloads }) =>
+//           `<li id=${id}><p>Likes: <span class='likes'>${likes}</span></p>`
+//       );
+//     })
+//     .then(console.log(object))
+//     .catch(error => console.log(error));
+// }
 // В ответе будет массив изображений удовлетворивших критериям параметров запроса.
 // Каждое изображение описывается объектом, из которого тебе интересны только
 // следующие свойства:
+// webformatURL - ссылка на маленькое изображение для списка карточек.
+// largeImageURL - ссылка на большое изображение.
+// tags - строка с описанием изображения. Подойдет для атрибута alt.
+// likes - количество лайков.
+// views - количество просмотров.
+// comments - количество комментариев.
+// downloads - количество загрузок.
 
 // Если бэкенд возвращает пустой массив, значит ничего подходящего найдено небыло.
 // В таком случае показывай уведомление с текстом "Sorry, there are no images
@@ -27,23 +118,25 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 // Шаблон разметки карточки одного изображения для галереи.
 
-// <div class="photo-card">
-//   <img src="" alt="" loading="lazy" />
-//   <div class="info">
-//     <p class="info-item">
-//       <b>Likes</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Views</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Comments</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Downloads</b>
-//     </p>
-//   </div>
-// </div>;
+{
+  /* <div class="photo-card">
+  <img src="" alt="" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+    </p>
+  </div>
+</div>; */
+}
 
 // ПАГИНАЦИЯ
 // Pixabay API поддерживает пагинацию и предоставляет параметры page и per_page.
@@ -94,3 +187,117 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 // Вместо кнопки «Load more» можно сделать бесконечную загрузку изображений при
 // прокрутке страницы.Мы предоставлям тебе полную свободу действий в реализации,
 // можешь использовать любые библиотеки.
+
+// const form = document.querySelector('form');
+// const list = document.querySelector('.list');
+// const button = document.querySelector('.load-more');
+
+// const inputField = document.querySelector('input');
+// // const fetchBtn = document.querySelector('button');
+// // const listOfGallery = document.querySelector('.gallery');
+// // const loadMoreBtn = document.querySelector('.load-more');
+
+// const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/events.json';
+// // const BASE_URL = 'https://pixabay.com/api/';
+// // const API = '30800169-3713389dad872250f057e0e33';
+// const API = '9cTjAjlRB53wyhAFk5VzXcBu5GiPU6fK';
+
+// let pageToFetch = 0;
+// let keyword = '';
+
+// function fetchEvent(page, keyword) {
+//   const params = new URLSearchParams({
+//     apikey: API,
+//     page,
+//     keyword,
+//     size: 50,
+//   });
+
+//   return fetch(`${BASE_URL}?${params}`)
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error(response.status);
+//       }
+//       return response.json();
+//     })
+//     .catch(error => console.log(error));
+// }
+
+// function getEvents(page, keyword) {
+//   fetchEvent(page, keyword).then(data => {
+//     console.log(data.page.totalElements);
+
+//     if (data.page.totalElements === 0) {
+//       button.classList.add('invisible');
+//       alert(`There are no events by keyword ${keyword}`);
+//     }
+
+//     const events = data?._embedded?.events;
+//     if (events) {
+//       renderEvents(events);
+//     }
+
+//     if (pageToFetch === data.page.totalPages - 1) {
+//       button.classList.add('invisible');
+//       alert('Finish');
+//       return;
+//     }
+//     pageToFetch += 1;
+//     if (data.page.totalPages > 1) {
+//       button.classList.remove('invisible');
+//     }
+//   });
+// }
+
+// // function renderEvents(events) {
+// //   const markup = events
+// //     .map(({ name, images }) => {
+// //       return `<li>
+// //     <img src='${images[0].url}' alt='${name}' width='200'>
+// //     <p>${name}</p>
+// //     </li>`;
+// //     })
+// //     .join('');
+// //   list.insertAdjacentHTML('beforeend', markup);
+// // }
+// function renderEvents(events) {
+//   const markup = events
+//     .map(({ name, images }) => {
+//       return `<li><div class="photo-card">
+//   <img src='${images[0].url}' alt='${name}' width='200' loading="lazy" />
+//   <div class="info">
+//     <p class="info-item">
+//       <b>Likes</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Views</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Comments</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Downloads</b>
+//     </p>
+//   </div>
+// </div></li>`;
+//     })
+//     .join('');
+//   list.insertAdjacentHTML('beforeend', markup);
+// }
+
+// form.addEventListener('submit', event => {
+//   event.preventDefault();
+//   // const query = event.target.elements.query.value;
+//   const query = inputField.value;
+//   keyword = query;
+//   pageToFetch = 0;
+//   list.innerHTML = '';
+//   if (!query) {
+//     return;
+//   }
+//   getEvents(pageToFetch, query);
+// });
+
+// button.addEventListener('click', () => {
+//   getEvents(pageToFetch, keyword);
+// });
